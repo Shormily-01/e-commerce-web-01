468f797a93574fbaa02dbd5921be95837e16b968
Jannatul Mawa Shormily <99941363+Shormily-01@users.noreply.github.com>
2022-07-21 12:03:31 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.tasty_pancake_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    prevArrow:'<span class="priv_arrow"><i class="fa-solid fa-angle-left"></i></span>',
+    nextArrow:'<span class="next_arrow"><i class="fa-solid fa-angle-right"></i></span>',
+  });
+  $('.sandwich_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:'<span class="priv_arrow"><i class="fa-solid fa-angle-left"></i></span>',
+    nextArrow:'<span class="next_arrow"><i class="fa-solid fa-angle-right"></i></span>',
+  });
